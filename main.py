f108dd77dd89060ee22d7a581b4446f0705a4e49
전은형 <ooooo5h91@gmail.com>
2021-12-31 23:35:28 +0900

    게시글 목록 조회 기능
    - 몇 페이지를 보고 싶은지 입력받아서 보여주기를 구현할 예정

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def show_main_menu():
     while True:
         print('======= 강의 관리 시스템 (LMS) =======')
         print('1. 수강생 목록 조회')
+        print('2. 게시글 목록 조회')
         print('0. 프로그램 종료')
         print('=====================================')
         num = int(input('메뉴 선택 : '))
@@ -13,12 +14,18 @@ def show_main_menu():
         if num == 0:
             print('프로그램을 종료합니다.')
             break
+        
         elif num == 1:
             get_user_list_from_db()
+            
+        elif num == 2:       
+            page_num = int(input('몇 페이지를 보시겠습니까? : '))
+            get_posts_by_page_from_db(page_num)
     
 
 # 1번 누르면, DB에서 수강생 목록 조회를 요청하는 기능
 def get_user_list_from_db():
+    
     result = get_user_list() 
   
     for row in result:
@@ -26,4 +33,11 @@ def get_user_list_from_db():
         user = Users(row)
         user.get_simple_info()  # dictionary가 Users 객체로 변환되었는지 확인
         
+        
+# 2번 누르면, DB에서 게시글을 페이지에 맞게 요청하는 기능
+def get_posts_by_page_from_db(page):
+    pass
+
+
+
 show_main_menu()
