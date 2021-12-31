26a422243996eb03e11980064f06453f987c8dcb
전은형 <ooooo5h91@gmail.com>
2021-12-31 23:27:23 +0900

    ????년 ?월 ?일로 datetime을 str로 가공

--- models/users.py
+++ models/users.py
@@ -14,13 +14,17 @@ class Users:
         
     
     # 사용자 정보를 간단하게 가공해서 출력
-    # 이름 - ?세, 성별
+    # 이름 - ?세, 성별 (2021년 8월 5일 가입)
     def get_simple_info(self):
         print(f'{self.name} - {self.get_age()}세, {self.gender}')
+        
+        # datetime 양식을 우리가 원하는 형식의 str로 가공해보자
+        temp_str = self.created_at.strftime('%Y년 %m월 %d일')
+        print(f'가입일자 : {temp_str}')
         
     
     # 사용자의 나이 출력
     def get_age(self):
         
-        current_year = datetime.datetime.now().year
+        current_year = datetime.datetime.utcnow().year
         return current_year - self.birth_year + 1
