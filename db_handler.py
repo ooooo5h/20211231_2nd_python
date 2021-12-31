dbd28783536ef49244acebe9e1ded0c5aa8ec135
전은형 <ooooo5h91@gmail.com>
2021-12-31 23:38:26 +0900

    각 페이지별 게시글 불러내는 쿼리 작성 완료

--- db_handler.py
+++ db_handler.py
@@ -23,3 +23,15 @@ def get_user_list():
     result = cursors.fetchall()
     
     return result
+
+
+# 페이지에 맞는 게시글 목록 가져오기
+def get_posts(page):
+    
+    offset = (page - 1) * 5
+    sql = f"SELECT * FROM posts AS p ORDER BY p.created_at DESC LIMIT {offset}, 5"
+    
+    cursors.execute(sql)
+    result = cursors.fetchall()
+    
+    return result
